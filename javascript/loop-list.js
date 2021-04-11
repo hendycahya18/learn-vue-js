f457fd7cfb1f181ca60f20e6ba864722db63d437
hendycahya18 <hendycahya@smakbo.sch.id>
2021-04-11 18:49:34 +0700

    Belajar loop list, membuat loop-list.html dengan script eksternal loop-list.js

---
+++ javascript/loop-list.js
@@ -0,0 +1,14 @@
+var vo = new Vue({
+    el: '#app',
+    data: {
+        members: [
+            { nama: "Hendy Cahya", umur: 19},
+            { nama: "Fathir Ihsan", umur: 9},
+            { nama: "Denny Wahyu", umur: 16}
+        ],
+        personels: {
+            nama: "Hendy Cahya", 
+            umur: 19
+        }
+    }
+})
