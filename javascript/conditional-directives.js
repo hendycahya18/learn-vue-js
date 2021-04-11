357af380d5023d19ba02e4cb99f8bc6c4567b1ef
hendycahya18 <hendycahya@smakbo.sch.id>
2021-04-11 19:09:13 +0700

    Migrasi dari vue 2 ke vue 3 pada loop-list.html dan conditional-directives.html, dan membuat vue js eksternal pada sisa file html lain, serta menghapus vue.js

--- javascript/conditional-directives.js
+++ javascript/conditional-directives.js
@@ -1,20 +1,25 @@
-var vo = new Vue({
-    el: '#app',
-    data: {
-        nama :'Hendy',
-        umur : 0,
-        done : false
+const vo = {
+    data() {
+        return{
+            nama :'Hendy',
+            umur : 0,
+            done : false
+        }
     },
-    computed: {
-        biodata:{
-            get: function(){
-                return this.nama + " umurnya " + this.umur;
-            },
-            set: function(params){
-                var cridential = params.split(' ')
-                this.nama = cridential[0]
-                this.umur = cridential[1]
+    computed() {
+        return{
+            biodata:{
+                get: function(){
+                    return this.nama + " umurnya " + this.umur;
+                },
+                set: function(params){
+                    var cridential = params.split(' ')
+                    this.nama = cridential[0]
+                    this.umur = cridential[1]
+                }
             }
         }
     }
-});
+};
+
+Vue.createApp(vo).mount('#app')
--- javascript/loop-list.js
+++ javascript/loop-list.js
@@ -1,14 +1,17 @@
-var vo = new Vue({
-    el: '#app',
-    data: {
-        members: [
-            { nama: "Hendy Cahya", umur: 19},
-            { nama: "Fathir Ihsan", umur: 9},
-            { nama: "Denny Wahyu", umur: 16}
-        ],
-        personels: {
-            nama: "Hendy Cahya", 
-            umur: 19
+const vo = {
+    data() {
+        return{
+            members: [
+                { nama: "Hendy Cahya", umur: 19},
+                { nama: "Fathir Ihsan", umur: 9},
+                { nama: "Denny Wahyu", umur: 16}
+            ],
+            personels: {
+                nama: "Hendy Cahya", 
+                umur: 19
+            }
         }
     }
-})
+}
+
+Vue.createApp(vo).mount('#app')
