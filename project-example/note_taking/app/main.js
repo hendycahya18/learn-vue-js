7b469490ad532441495b67ca7728a0afdc42280f
hendycahya18 <hendycahya@smakbo.sch.id>
2021-04-25 14:38:49 +0700

    Finish learn vuex in static vue

--- project-example/note_taking/app/main.js
+++ project-example/note_taking/app/main.js
@@ -1,11 +1,86 @@
-const inputComponent = {
-  template: `<input placeholder='Enter a note' class="input is-small" type="text" />`,
+const state = {
+  notes: [],
+  timestamps: [],
 }
 
-const app = {
-  components: {
-    'input-component': inputComponent
+const mutations = {
+  ADD_NOTE(state, payload) {
+    let newNote = payload;
+    state.notes.push(newNote);
+  },
+  ADD_TIMESTAMP(state, payload) {
+    let newTimeStamp = payload;
+    state.timestamps.push(newTimeStamp);
+  },
+}
+
+const actions = {
+  addNote(context, payload) {
+    context.commit('ADD_NOTE', payload);
+  },
+  addTimestamp(context, payload) {
+    context.commit('ADD_TIMESTAMP', payload);
+  },
+}
+
+const getters = {
+  getNotes: state => state.notes,
+  getTimestamps: state => state.timestamps,
+  getNoteCount: state => state.notes.length,
+}
+
+const store = Vuex.createStore({
+  state: state,
+  mutations: mutations,
+  actions: actions,
+  getters: getters,
+})
+
+const inputComponent = {
+  template: `<input
+    placeholder='Enter a note'
+    v-model="input"
+    @keyup.enter="monitorEnterKey"
+    class="input is-small" type="text" />`,
+  data () {
+    return {
+      input: '',
+    }
+  },
+  methods:{
+    monitorEnterKey(){
+      this.$store.dispatch('addNote', this.input);
+      this.$store.dispatch('addTimestamp', new Date().toLocaleString());
+      this.input= '';
+    }
   }
 }
 
-Vue.createApp(app).mount('#app')
+const noteCountComponent = {
+  template:`<div class="note-count"> Note count: <strong>{{ noteCount }}</strong> </div>`,
+  computed: {
+    noteCount() {
+      return this.$store.getters.getNoteCount;
+    }
+  },
+}
+
+
+const app = Vue.createApp({
+  computed: {
+    notes() {
+      return this.$store.getters.getNotes;
+    },
+    timestamps() {
+      return this.$store.getters.getTimestamps;
+    },
+  },
+  components: {
+    'input-component': inputComponent,
+    'note-count-component': noteCountComponent,
+  },
+})
+
+app.use(store)
+app.mount('#app')
+
